b776df765372424ad89e4e35cec4adf0208ab8cd
bitongRep <3287432883@qq.com>
2020-07-05 17:18:05 +0800

    枚举类包括

---
+++ gradleTest/src/main/java/com/pdd.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @program: gitTest
+ * @author: Bi tong
+ * @create: 2020-06-30 11:20
+ **/
+public class pdd {
+    public static void main(String[] args) {
+        System.out.println("gradle Test");
+    }
+}
